e9f84b59106cde55d5a8867beea7b45bf015d980
Jihed Mannai <jihed.mannai@eprit.tn.com>
2022-04-10 05:03:26 +0100

    commit matieres prem v2

--- connection.cpp
+++ connection.cpp
@@ -8,9 +8,9 @@ connection::connection()
 bool connection::createConnection(){
    bool test=false;
    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-   db.setDatabaseName("construction");
-   db.setUserName("Omar");//inserer nom de l'utilisateur
-   db.setPassword("esprit22");//inserer mot de passe de cet utilisateur
+   db.setDatabaseName("matieresPrem");
+   db.setUserName("jihed");//inserer nom de l'utilisateur
+   db.setPassword("jihed");//inserer mot de passe de cet utilisateur
    if(db.open())
 test=true;
 return test;
